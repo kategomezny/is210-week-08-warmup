6b82849b723f257be4c82bd8ac017fcb2dd5777d
kategomezny <35971954+kategomezny@users.noreply.github.com>
2018-03-24 18:23:49 -0400

    Update task_03.py

--- task_03.py
+++ task_03.py
@@ -6,3 +6,9 @@ EXPENSE = 14.23
 LOOKS_NICE = True
 MAX_EXPENSE = 12
 GET_OUT_ALIVE = False
+
+if (LOOKS_NICE == True or EXPENSE <= MAX_EXPENSE):
+    SACRIFICE = True
+
+elif GET_OUT_ALIVE == False:
+    SACRIFICE = False
